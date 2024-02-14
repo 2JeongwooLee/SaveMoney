890c61a6c2a35831d258497c43c5010966d03761
JeongwooLee <wjddn3751@gmail.com>
2024-02-15 00:57:56 +0900

    refactor. 상품 조회 페이징 처리

--- src/main/java/com/master/savemoney/goods/repository/GoodsRepository.java
+++ src/main/java/com/master/savemoney/goods/repository/GoodsRepository.java
@@ -2,10 +2,14 @@ package com.master.savemoney.goods.repository;
 
 import com.master.savemoney.goods.entity.Goods;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface GoodsRepository extends JpaRepository<Goods, Long> {
 
-  List<Goods> findByNameContaining(@Param("searchName") String searchName);
+  Page<Goods> findByNameContaining(@Param("searchName") String searchName, Pageable pageable);
+
+  Page<Goods> findAll(Pageable pageable);
 }
